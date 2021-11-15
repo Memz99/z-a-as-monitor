5e87fcc705407754b69fd2983998f9d639f9201c
Salvydas Lukosius <sall@w-ss.io>
2021-11-15 19:00:51 +0000

    Update z-a-as-monitor.plugin.zsh

--- z-a-as-monitor.plugin.zsh
+++ z-a-as-monitor.plugin.zsh
@@ -2,7 +2,7 @@
 # License MIT
 
 # According to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+# http://z-shell.github.io/ZSH-TOP-100/Zsh-Plugin-Standard.html
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
@@ -16,7 +16,4 @@ autoload -Uz →za-as-monitor-preinit-handler
     hook:preinit \
     →za-as-monitor-preinit-handler \
     →za-as-monitor-help-null-handler \
-    "dlink''|.monitor''" # The ice conflict with dl'' from z-a-patch-dl is being handled
-                       # in the other annex
-
-# vim:ft=zsh:tw=80:sw=4:sts=4:et
+    "dlink''|.monitor''" # The ice conflict with dl'' from z-a-patch-dl is being handled in the other annex
